ccf8eb5da0bd46fd3b879faceb29d02c6f23e03d
Carlo Maria Calcagni <turbidvector30@gmail.com>
2022-02-15 10:35:18 +0100

    first push

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emulatore_Pdp8
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false); //things di form che per ora non mi servono
+            Application.Run(new Form1());*/
+            Console.WriteLine("hello world!\n");
+        }
+    }
+}
