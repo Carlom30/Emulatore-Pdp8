4a99095ea9c821dd665d572f4bd2aa98ef6118df
Carlo Maria Calcagni <turbidvector30@gmail.com>
2022-02-15 19:50:10 +0100

    pdp8 class declaration and partially definition. definition of 12-bit and 16-bit types.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Emulatore_Pdp8
+
+namespace Emulatore_Pdp8 //già definito da un'altra parte??
+
 {
     static class Program
     {
@@ -20,4 +22,58 @@ namespace Emulatore_Pdp8
             Console.WriteLine("hello world!\n");
         }
     }
+
+    struct i16
+    {
+        short value;
+    }
+
+    struct u12
+    {
+        private ushort _value; //la cosina sotto l'ha detta endu
+
+        public u12(ushort firstValue)
+        {
+            _value = 0;
+            setValue(firstValue);
+        }
+
+        public ushort getValue()
+        {
+            return (ushort)(_value & ((ushort)0b_0000_1111_1111_1111));
+        }
+
+        public void setValue(ushort newValue)
+        {
+            if(newValue >= (1 << 12))
+            {
+                Console.WriteLine("overflow!\n");
+                return; //crash probabilmente
+            }
+
+            _value = newValue;
+        }
+
+    }
+
+    class pdp8
+    {
+        //definizione contenuto della macchina e funzioni istruzioni
+        i16[] ram; //insieme di tutti i registri (eccetto quelli sotto)
+        i16 mbr; // memory buffer register
+        i16 a; //accumulatore
+
+        u12 mar; //memory address register
+        u12 pc; //program counter
+
+        bool e;
+
+        //insieme dei flip-flop per tipo di indirizzamento
+        bool s;
+        bool f;
+        bool r;
+
+    }
+
+    
 }
